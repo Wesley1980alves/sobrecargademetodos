027c6190a5171eec14fdd12ce35215fe5f481081
wesleyalves <110191958+Wesley1980alves@users.noreply.github.com>
2022-10-16 19:44:57 -0300

    polimorfismo
    atividade corrigida

--- src/sobrecargademetodos/TesteCalculadora.java
+++ src/sobrecargademetodos/TesteCalculadora.java
@@ -9,7 +9,7 @@ public class TesteCalculadora {
 		System.out.println(calc.soma(120, 20, 35));
 		System.out.println(calc.soma(125, 3, 15, 154));
 		System.out.println(calc.soma("Nao deixa morrer a ", "flor da vida" ));
-		System.out.println(calc.soma("Jamais desprese a vida\n", "Nao importa o qual dificil seja", "\nAte as flores caim em algum momento", "\nEntao lembre-se cair ,nao e sinal de derrota  ", "\nLevante-se e continui lutando que com o tempo florescera"));
+		System.out.println(calc.soma("Jamais desprese a vida\n", "Nao importa o quanto dificil seja", "\nAte as flores caim em algum momento", "\nEntao lembre-se cair ,nao e sinal de derrota  ", "\nLevante-se e continui lutando que com o tempo florescera"));
 
 	}
 
